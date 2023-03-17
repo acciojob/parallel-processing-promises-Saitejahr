ad6d2a4830efd3b424225b31d96f99efeacd4709
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-17 13:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //your JS code here. If required.
 const images = [
-    { url: 'https://picsum.photos/id/1/200/300' },
-    { url: 'https://picsum.photos/id/2/200/300' },
-    { url: 'https://picsum.photos/id/3/200/300' },
-    { url: 'https://picsum.photos/id/4/200/300' },
-    { url: 'https://picsum.photos/id/5/200/300' },
-    { url: 'https://picsum.photos/id/6/200/300' },
-    { url: 'https://picsum.photos/id/7/200/300' },
-    { url: 'https://picsum.photos/id/8/200/300' },
-    { url: 'https://picsum.photos/id/9/200/300' },
+    { url: 'https://picsum.photos/id/12/200/300' },
+    { url: 'https://picsum.photos/id/11/200/300' },
     { url: 'https://picsum.photos/id/10/200/300' },
+    { url: 'https://picsum.photos/id/14/200/300' },
+    { url: 'https://picsum.photos/id/13/200/300' },
+    { url: 'https://picsum.photos/id/16/200/300' },
+    { url: 'https://picsum.photos/id/17/200/300' },
+    { url: 'https://picsum.photos/id/18/200/300' },
+    { url: 'https://picsum.photos/id/19/200/300' },
+    { url: 'https://picsum.photos/id/20/200/300' },
   ];
   
   function downloadImages() {
